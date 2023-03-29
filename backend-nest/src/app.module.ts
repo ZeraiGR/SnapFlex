54e7ab909cd2dceae9451e26a73d404c24972f97
ZeraiGR <kirillgr15@mail.ru>
2023-03-29 18:15:17 +0300

    mongoose module was installed for root

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from './configs/mongo.config';
+import { AuthModule } from './auth/auth.module';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig,
+    }),
+    AuthModule,
+  ],
+})
 export class AppModule {}
