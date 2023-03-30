586f0d29405afbaf213eed0f152438c26a7c7ef5
ZeraiGR <kirillgr15@mail.ru>
2023-03-30 18:16:58 +0300

    jwt strategies were implemented

---
+++ backend-nest/src/auth/strategies/index.ts
@@ -0,0 +1,2 @@
+export * from './ar.strategy';
+export * from './rt.strategy';
---
+++ backend-nest/src/auth/strategies/rt.strategy.ts
@@ -0,0 +1,33 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+
+import { JwtPayload, JwtPayloadWithRt } from '../types';
+import { WRONG_REFRESH_TOKEN } from '../auth.constants';
+
+@Injectable()
+export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get<string>('RT_SECRET'),
+      passReqToCallback: true,
+    });
+  }
+
+  validate(req: Request, payload: JwtPayload): JwtPayloadWithRt {
+    const refreshToken = req
+      ?.get('authorization')
+      ?.replace('Bearer', '')
+      .trim();
+
+    if (!refreshToken) throw new ForbiddenException(WRONG_REFRESH_TOKEN);
+
+    return {
+      ...payload,
+      refreshToken,
+    };
+  }
+}
