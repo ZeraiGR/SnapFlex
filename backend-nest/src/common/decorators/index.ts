15c45cf6b20cdac3e473687090c2d7175532d0f0
ZeraiGR <kirillgr15@mail.ru>
2023-03-30 21:16:16 +0300

    new decorators were implemented

--- backend-nest/src/common/decorators/index.ts
+++ backend-nest/src/common/decorators/index.ts
@@ -1,3 +1,5 @@
+export * from './email-not-registered.decorator';
+export * from './username-not-registered.decorator';
 export * from './get-current-user.decorator';
 export * from './get-current-user-id.decorator';
 export * from './match.decorator';
