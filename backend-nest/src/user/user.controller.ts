35e532258b72b7d2b99fa36701a1e69a35e60f37
ZeraiGR <kirillgr15@mail.ru>
2023-04-04 01:00:16 +0300

    user controller was updated

--- backend-nest/src/user/user.controller.ts
+++ backend-nest/src/user/user.controller.ts
@@ -3,6 +3,7 @@ import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { USER_NOT_FOUNT_ERROR } from './user.constants';
 import { UserService } from './user.service';
 import { UserDocument } from './schemas';
+import { IdValidationPipe } from 'src/common/pipes/id.validation.pipe';
 
 @Controller('users')
 export class UserController {
@@ -14,13 +15,15 @@ export class UserController {
   }
 
   @Get(':id')
-  async findById(@Param('id') id: string): Promise<UserDocument> {
+  async findById(
+    @Param('id', IdValidationPipe) id: string,
+  ): Promise<UserDocument> {
     const user = await this.userService.findById(id);
     if (!user) throw new NotFoundException(USER_NOT_FOUNT_ERROR);
     return user;
   }
 
-  @Get(':email')
+  @Get('byEmail/:email')
   async findByEmail(@Param('email') email: string): Promise<UserDocument> {
     const user = await this.userService.findByEmail(email);
     if (!user) throw new NotFoundException(USER_NOT_FOUNT_ERROR);
