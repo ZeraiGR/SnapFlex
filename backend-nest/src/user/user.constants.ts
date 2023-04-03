47c850ebbdcacc5774169ed5437c83dbf2fd8e5d
ZeraiGR <kirillgr15@mail.ru>
2023-04-04 00:59:58 +0300

    user constants were updated

--- backend-nest/src/user/user.constants.ts
+++ backend-nest/src/user/user.constants.ts
@@ -1,1 +1,3 @@
 export const USER_NOT_FOUNT_ERROR = 'Такой пользователь не найден';
+export const IS_NOT_EMPTY_ERROR = 'Данное поле обязательно к заполнению';
+export const IS_NOT_STRING_ERROR = 'Данное поле должно быть строкой';
