4f847c3cd13947a23d421d562b354ece1ddf8188
ZeraiGR <kirillgr15@mail.ru>
2023-04-04 00:56:12 +0300

    card constants was implemented

---
+++ backend-nest/src/card/card.constants.ts
@@ -0,0 +1,8 @@
+export const CARD_NOT_FOUNT_ERROR = 'Такая карточка не найдена';
+export const IS_NOT_EMPTY_ERROR = 'Данное поле обязательно к заполнению';
+export const IS_NOT_STRING_ERROR = 'Данное поле должно быть строкой';
+export const INCORRECT_CATEGORY_ERROR = 'Такой категории не существует';
+export const USER_NOT_FOUND_ERROR = 'Такой пользователь не найден';
+export const ID_IS_NOT_CORRECT_ERROR = 'Некорректный ID';
+export const CARD_IS_NOT_AVAILABLE_ERROR =
+  'Данная карточка сейчас недоступна для повторения';
