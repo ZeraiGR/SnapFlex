31537932b6dafd2a20544d30d99f712f4c95127d
ZeraiGR <kirillgr15@mail.ru>
2023-05-25 19:09:30 +0300

    app component was updated with theme context

--- frontend-next/src/pages/_app.tsx
+++ frontend-next/src/pages/_app.tsx
@@ -1,6 +1,48 @@
-import '@/styles/globals.css';
+import { useMemo, useState } from 'react';
+import { CacheProvider, EmotionCache } from '@emotion/react';
 import type { AppProps } from 'next/app';
+import { PaletteMode } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Head from 'next/head';
+import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { ThemeContext } from '@/context/themeContext';
+import { getDesignTokens, createEmotionCache } from '@/utils';
+
+const clientSideEmotionCache = createEmotionCache();
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function App({ Component, emotionCache = clientSideEmotionCache, pageProps }: MyAppProps) {
+  const [mode, setMode] = useState<PaletteMode>('dark');
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode: PaletteMode) =>
+          prevMode === 'light' ? 'dark' : 'light',
+        );
+      },
+    }),
+    [],
+  );
+
+  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <Head>
+            <meta name="viewport" content="initial-scale=1, width=device-width" />
+          </Head>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ThemeContext.Provider>
+    </CacheProvider>
+  );
 }
