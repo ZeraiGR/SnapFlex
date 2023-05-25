d3186120f265deb50bd2612745eff47b5248ded6
ZeraiGR <kirillgr15@mail.ru>
2023-05-25 19:57:31 +0300

    footer was updated

--- frontend-next/src/layout/Footer.tsx
+++ frontend-next/src/layout/Footer.tsx
@@ -1,11 +1,24 @@
-interface FooterProps {
-  something: string;
-}
+import Typography from '@mui/material/Typography';
+import Image from 'next/image';
 
-export default function Footer(props: FooterProps) {
+export default function Footer() {
   return (
-    <footer>
-      my footer
+    <footer style={{
+      backgroundColor: '#202020',
+      padding: '80px 20px 120px'
+    }}>
+      <Image
+        src="/icons/logo-white.svg"
+        width={175}
+        height={46}
+        style={{
+          margin: '0 auto 60px'
+        }}
+        alt="Footer logo"
+      />
+      <Typography variant="body2" color="#ffffff" align="center">
+      {'©' + new Date().getFullYear()}. All rights reserved
+    </Typography>
     </footer>
   );
 }
